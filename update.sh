388de35ce1e7b5265c909c1aa9d100b06d4a6c53
Jonny Weese <jonny@cs.jhu.edu>
2014-01-15 15:59:53 -0500

    update.sh: exit 0 if leaderboard.js hasn't changed

--- update.sh
+++ update.sh
@@ -4,10 +4,13 @@ cd $HOME/code/mt-class
 ./scripts/download-all.pl
 ./scripts/build-table.pl > leaderboard.js
 
-diff leaderboard.js $HOME/public_html/mt-class/leaderboard.js > /dev/null
-if [[ $? -eq 1 ]]; then
-    stamp=$(date +"%F-%H-%M")
-    cat $HOME/public_html/mt-class/leaderboard.js | perl -pe 's/var data/var olddata/' > $HOME/public_html/mt-class/leaderboard.js.$stamp
-    ln -sf leaderboard.js.$stamp $HOME/public_html/mt-class/leaderboard-old.js
-    mv leaderboard.js $HOME/public_html/mt-class/leaderboard.js
+if diff leaderboard.js $HOME/public_html/mt-class/leaderboard.js > /dev/null
+then
+	# leaderboard.js hasn't changed.
+	exit 0
 fi
+
+stamp=$(date +"%F-%H-%M")
+cat $HOME/public_html/mt-class/leaderboard.js | perl -pe 's/var data/var olddata/' > $HOME/public_html/mt-class/leaderboard.js.$stamp
+ln -sf leaderboard.js.$stamp $HOME/public_html/mt-class/leaderboard-old.js
+mv leaderboard.js $HOME/public_html/mt-class/leaderboard.js
