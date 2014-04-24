63154f1da5a0a6449a0238a4610fd3391fa82fcd
Matt Post <post@cs.jhu.edu>
2014-04-24 09:53:12 -0400

    Fixed script to score both dev and test data

--- scoring/inflect.py
+++ scoring/inflect.py
@@ -7,11 +7,15 @@ inflect_data/dtest.form (development) or inflect_data/etest.form (post-deadline 
 """
 
 import os
+import sys
 import codecs
 import datetime
 from itertools import izip
 
 from google.appengine.ext import ndb
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 reverse_order = True
 deadline = datetime.datetime(2014, 04, 28, 23, 00)
@@ -29,8 +33,15 @@ def score(e_file, assignment_key, test=False):
 
     total = 0
     right = 0
-    for line, gold in izip(e_file.strip().split('\n'), codecs.open(goldfile, 'r', 'utf-8')):
-        compared = map(lambda x: x[0] == x[1], izip(line.split(), gold.split()))
+    num = 0
+
+    if test:
+        data = e_file.split('\n')[4042:]
+    else:
+        data = e_file.split('\n')[:4042]
+
+    for line, gold in izip(data, codecs.open(goldfile, 'r', 'utf-8')):
+        compared = map(lambda x: x[0] == x[1], izip(line.strip().split(), gold.strip().split()))
         right += sum(compared)
         total += len(compared)
 
