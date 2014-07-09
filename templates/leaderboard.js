9e8a772a6a3f749d7a786e48fd9694d93cb5cb2d
Matt Post <post@cs.jhu.edu>
2014-07-09 15:58:56 -0400

    Added a decimal point

--- templates/leaderboard.js
+++ templates/leaderboard.js
@@ -1,6 +1,6 @@
 var data = [
 {% for user in handles %}
-  ["{{user}}" {% for score in scores[user] %} , "{{ '%0.2f' % (score)}}" {% endfor %} ],
+  ["{{user}}" {% for score in scores[user] %} , "{{ '%0.3f' % (score)}}" {% endfor %} ],
 {% endfor %}
 ];
 
